df3eb88bdda9cd6816588782cd26f268f21402da
teobryer <teobryer@h001-3>
2020-01-15 17:07:23 +0100

    afficher lignes

--- tp1/creer_tableau.php
+++ tp1/creer_tableau.php
@@ -14,6 +14,29 @@ function prixtaux($n)
 
 
 $b = array_map('prixtaux', array_fill_keys($articles, 0));
-print_r($b);
+//print_r($b);
 
+echo "<table class=".'table'.">
+<thead>
+<tr>
+<th scope=".'col'.">Article</th>
+<th scope=".'col'.">Prix</th>
+<th scope=".'col'.">Taux TVA</th>
+<th scope=".'col'.">Coût HT</th>
+<th scope=".'col'.">Coût TTC</th>
+</tr>
+</thead>
+<tbody>";
+    
+foreach ($b as $key => $ligne){
+  echo " <tr>
+    <th scope=".'row'.">".$key."</th>
+    <td>".$ligne["Prix"]."</td>
+    <td>".$ligne["Taux"]."</td>
+    <td>".$ligne["Prix"]."</td>
+    <td>".($ligne["Prix"]+$ligne["Prix"]*$ligne["Taux"])."</td>
+    </tr>";
+}
+
+echo" </tbody> </table>";
 ?>
